5cf3785ed4a3c58a63debc1a344e14748cbee89e
Jayhun Rahim <jayhun9999@gmail.com>
2025-11-24 17:19:45 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace OTEL_BİLGİ_SİSTEMİ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtbSonuclar = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtArama
+            // 
+            this.txtArama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtArama.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtArama.Location = new System.Drawing.Point(31, 308);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(877, 28);
+            this.txtArama.TabIndex = 0;
+            this.txtArama.TextChanged += new System.EventHandler(this.txtArama_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Source Sans Pro", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label1.Location = new System.Drawing.Point(1149, 594);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hi !!! ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Source Sans Pro", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label2.Location = new System.Drawing.Point(995, 631);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(388, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "I\'m GLORİA HOTELS AND RESORTS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Source Sans Pro", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label3.Location = new System.Drawing.Point(1079, 699);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "What would you like";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Source Sans Pro", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label4.Location = new System.Drawing.Point(1086, 731);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "to know about me?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Source Sans Pro", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label5.Location = new System.Drawing.Point(1081, 810);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "* Write and Search *";
+            // 
+            // rtbSonuclar
+            // 
+            this.rtbSonuclar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtbSonuclar.Location = new System.Drawing.Point(31, 368);
+            this.rtbSonuclar.Name = "rtbSonuclar";
+            this.rtbSonuclar.Size = new System.Drawing.Size(877, 473);
+            this.rtbSonuclar.TabIndex = 9;
+            this.rtbSonuclar.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OTEL_BİLGİ_SİSTEMİ.Properties.Resources.GLORİA;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1372, 244);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackgroundImage = global::OTEL_BİLGİ_SİSTEMİ.Properties.Resources.SEARCH;
+            this.btnAra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAra.FlatAppearance.BorderSize = 0;
+            this.btnAra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAra.Location = new System.Drawing.Point(914, 368);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(102, 106);
+            this.btnAra.TabIndex = 2;
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::OTEL_BİLGİ_SİSTEMİ.Properties.Resources.pngtree_little_cute_robot_funny_virtual_assistant_bot_png_image_11549965;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1040, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 307);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1452, 871);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbSonuclar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtArama);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GLORİA DİJİTAL ASSİSTAN";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtbSonuclar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OTEL_BİLGİ_SİSTEMİ
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=JAYHUN;Initial Catalog=OTEL BİLGİ SİSTEMİ; Integrated Security = True; TrustServerCertificate=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtArama_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string kelime = txtArama.Text.Trim();
+            rtbSonuclar.Clear(); // RichTextBox'ı temizle
+
+            if (kelime.Length < 2)
+            {
+                rtbSonuclar.Text = "Please select minimum 2 characters!";
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sorgu = "SELECT Baslik, Icerik FROM Bilgiler WHERE Baslik LIKE @kelime OR Icerik LIKE @kelime OR Etiketler LIKE @kelime";
+                SqlCommand cmd = new SqlCommand(sorgu, conn);
+                cmd.Parameters.AddWithValue("@kelime", "%" + kelime + "%");
+
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    // Başlığı KALIN ve MAVİ yap
+                    rtbSonuclar.SelectionFont = new Font(rtbSonuclar.Font, FontStyle.Bold);
+                    rtbSonuclar.SelectionColor = Color.Blue;
+                    rtbSonuclar.AppendText(dr["Baslik"].ToString() + "\n");
+
+                    // İçeriği normal siyah yazı
+                    rtbSonuclar.SelectionFont = new Font(rtbSonuclar.Font, FontStyle.Regular);
+                    rtbSonuclar.SelectionColor = Color.Black;
+                    rtbSonuclar.AppendText(dr["Icerik"].ToString() + "\n\n");
+                }
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("MERHABA! Bilgiye mi ihtiyacın var?");
+            
+        }
+    }
+}
